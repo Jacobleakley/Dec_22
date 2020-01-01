ec1bcd52a968905d270e5ab045ac56b3fdec14c9
Jacobleakley <jacob.bleakley@gmail.com>
2019-12-31 22:47:51 -0500

    Arrays and Hashes exercises

---
+++ exercise10b.rb
@@ -0,0 +1,58 @@
+#1 
+
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+#2
+
+# def method(hash)
+# hash.each do |k, v|
+#   puts "#{k}: #{v} students"
+# end
+# end 
+
+# method(students)
+
+#3 
+
+# students[:cohort4] = 43
+
+#4 
+
+# print students.keys 
+
+#5
+
+# students.each do |k, v|
+#  students[k] = v * 1.05
+#   puts "#{k}: #{students[k].to_i} students"
+#   end 
+
+#6
+
+# students.delete(:cohort2)
+
+# puts students 
+
+# #7
+
+# a = 0
+# students.each do |k, v|
+# a += v.to_i
+# end 
+
+print a 
+
+
+
+
+
+
+
+
+
+
+
